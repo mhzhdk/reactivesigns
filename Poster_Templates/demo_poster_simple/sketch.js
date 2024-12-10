603fe74bed7f17d7f0885673338cb48311dabb4c
mhzhdk <mathan.hazazi@zhdk.ch>
2024-12-10 21:53:20 +0100

    lastShortChange

--- Poster_Templates/demo_poster_simple/sketch.js
+++ Poster_Templates/demo_poster_simple/sketch.js
@@ -1,22 +1,16 @@
 
 let gridCountX = 16;
-
 let gridCountY = 56;
-
 
 let gridArray = Array(gridCountX).fill().map(()=> Array(gridCountY).fill(null));
 
-let triangleList = []
+let triangleList = [] // this stores the actual TesoTriangle objects
 
 function setup() {
 
  /*important!*/ createCanvas(poster.getWindowWidth(), poster.getWindowHeight()); // Don't remove this line.
- 
-
- /*important!*/ poster.setup(this, "/Poster_Templates/libraries/assets/models/movenet/model.json");  // Don't remove this line.
-
+  /*important!*/ poster.setup(this, "/Poster_Templates/libraries/assets/models/movenet/model.json");  // Don't remove this line.
  createCanvas(1050/2, 1920/2);
-
  background(100);
 
  
@@ -37,13 +31,11 @@ function setup() {
 
 
 function draw() {
-
-
- noStroke();
+noStroke();
 
  for(let i = 0; i<triangleList.length; i++) {
 
-   triangleList[i].display();
+   triangleList[i].showTriangle();
 
  }
 
@@ -92,78 +84,37 @@ class TesoTriangle {
 
  constructor(i,j,w,h) {
 
-   this.i = i;
-
+   this.i = i; //row and column
    this.j = j;
 
-   this.w = width/gridCountX; 
-
+   this.w = width/gridCountX; // width and height -> dependant on canvas size
    this.h = height/gridCountY; 
 
-   this.x = i * this.w;
-
+   this.x = i * this.w; // top left corver coordinates of triangle
    this.y = j * this.h;  
 
-   this.active = false;
+   this.active = false; // boolean to determine color
 
  }
 
  
-
- display(){
-
-   if (this.active == true) {
-
-     fill(0);
-
-     gridArray[this.i][this.j] = 0;
-
-   } else {
-
-     fill(255);
-
-     gridArray[this.i][this.j] = 1;
-
-   }
-
-   this.showTriangle();
-
- }
-
-
  showTriangle(){
 
-   //  rect(this.x,this.y,this.w,this.h)
-
-   push();
+   push(); // for every triangle new settingBase
 
    translate(this.x,this.y)
 
-
+//calling the number arrays to color them
    let isWhite = numberThree.some(item => item[0] === this.i && item[1] === this.j);
-
-
-   // Set color based on whether the triangle should be white
-
-   if (isWhite) {
-
-     fill(0); // white
-
-   } else {
-
-     fill(255); // black
-
-   }
+   if (isWhite) {fill(0); } else {fill(255);} //coloring the numbers
 
 
    // work out the orientation of the triangle based on column and row. 
-
+   // checks if odd or even
    let invert = this.i%2
-
    invert -= this.j%2
 
    // draw
-
    if (invert) {
 
    triangle(0, this.h/2, this.w, 0-(this.h/2), this.w, this.h+(this.h/2));
