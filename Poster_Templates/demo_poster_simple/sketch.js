f168ab57ca3829501c1b31bfd584d382c8b63c31
mhzhdk <mathan.hazazi@zhdk.ch>
2024-12-11 11:34:53 +0100

    RandomizationTrue

--- Poster_Templates/demo_poster_simple/sketch.js
+++ Poster_Templates/demo_poster_simple/sketch.js
@@ -1,5 +1,6 @@
 
 let gridCountX = 16;
+let seed = 12345;
 let gridCountY = 56;
 let offset = 3;
 let random10 = [];
@@ -36,6 +37,7 @@ function setup() {
 
 
 function draw() {
+  randomSeed(seed)
   counter++
   if (counter==10){
     console.log(random10)
@@ -49,6 +51,9 @@ noStroke(); // IN REALIT
  for(let i = 0; i<triangleList.length; i++) 
 {
    triangleList[i].showTriangle();
+}
+for(let i =0; i < random10.length; i++){
+  random10[i].displayTriangle();
 }
 
 
@@ -97,6 +102,88 @@ class TesoTriangle
 
 
     showTriangle()
+    {
+
+      
+      push(); // for every triangle new settingBase
+
+      translate(this.x,this.y)
+
+
+      //calling the number arrays to color them
+      let isWhite = numberThree.some(item => item[0] === this.i && item[1] === this.j);
+      if (isWhite) 
+       {
+          fill(0); 
+          isBlackTriangles.push(this);
+        } 
+      else 
+        {
+        fill(255); 
+        } //coloring the numbers
+
+
+
+      
+      random10 = this.getRandomElements(isBlackTriangles, 10);
+
+
+
+
+      // work out the orientation of the triangle based on column and row. 
+      // checks if odd or even
+      let invert = this.i%2
+      invert -= this.j%2
+
+
+      let slider = map(mouseX,0,width,-20,20)
+      // draw
+      if (invert) 
+      {
+
+        let p1 = createVector(0, this.h/2);
+        let p2 = createVector(this.w, 0-(this.h/2))
+        let p3 = createVector(this.w, this.h+(this.h/2))
+
+        let m = (p2.y-p1.y)/(p2.x-p1.x)
+
+        let parallelStart = createVector(p1.x+slider, p1.y+slider);
+
+        let deltaX = this.w+100
+        let deltaY = m * deltaX
+        let parallelEnd = createVector(parallelStart.x + deltaX, parallelStart.y +deltaY)
+
+        let rectWidth = (parallelEnd.x - parallelStart.x)/2;
+        let rectHeight = (parallelEnd.y - parallelStart.y)/2;
+
+
+        triangle(p1.x,p1.y,p2.x,p2.y,p3.x,p3.y)
+
+      }
+
+      else 
+      {
+        let p4 = createVector(0, 0-(this.h/2))
+        let p5 = createVector(this.w, this.h/2)
+        let p6 = createVector(0, this.h+(this.h/2))
+
+        let m = -(p5.y-p4.y)/(p5.x-p4.x)
+        let parallelStart = createVector(p4.x-20, p4.y);
+
+        let deltaX = 200
+        let deltaY = m * deltaX
+        let parallelEnd = createVector(parallelStart.x + deltaX, parallelStart.y +deltaY)
+
+          triangle(p4.x,p4.y, p5.x,p5.y,p6.x,p6.y);
+
+      }
+  
+
+      pop();
+
+    }
+
+    displayTriangle()
     {
 
       
